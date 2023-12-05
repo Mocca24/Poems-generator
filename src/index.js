f55cd0ba91945e758c04ee6752d1e540c0ef91c8
Mocca24 <signupcyn@gmail.com>
2023-12-05 17:13:16 +0200

    fixed prompt and innerhtml

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "8a84a250d94e12ocbbetef4209917359";
   let context =
-    "You are a poetry expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do NOT include a title to the poem.";
+    "You are a poetry expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions and do not include a title to the poem.";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating"> Beautiful words loading... ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating"> Loading beautiful words about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
