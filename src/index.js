0fced01a4f5ec603f76e230ce18cfbc34a7ddb51
Mocca24 <signupcyn@gmail.com>
2023-12-05 16:11:24 +0200

    basics form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert ("hello world");
+function generatePoem(event) {
+    event.preventDefault();
+    
+    new Typewriter('#poem', {
+  strings: "something something",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+   
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);s
